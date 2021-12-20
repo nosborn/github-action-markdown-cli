7b698a33ed50e9625fe71c056fcf52b658766e4b
Nick Osborn <nosborn@users.noreply.github.com>
2021-12-20 12:02:00 +0800

    Use a problem matcher

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,41 +6,12 @@ MARKDOWNLINT="${MARKDOWNLINT}${INPUT_IGNORE_FILES:+ -i ${INPUT_IGNORE_FILES}}"
 MARKDOWNLINT="${MARKDOWNLINT}${INPUT_IGNORE_PATH:+ -p ${INPUT_IGNORE_PATH}}"
 MARKDOWNLINT="${MARKDOWNLINT}${INPUT_RULES:+ -r ${INPUT_RULES}}"
 
+echo '::add-matcher::.github/markdownlint-problem-matcher.json'
+
 # shellcheck disable=SC2086
-OUTPUT=$(${MARKDOWNLINT} ${INPUT_FILES} 2>&1)
-SUCCESS=$?
-echo "${OUTPUT}"
+${MARKDOWNLINT} ${INPUT_FILES}
+readonly RC=$?
 
-if [ ${SUCCESS} -eq 0 ]; then
-  exit 0
-fi
+echo '::remove-matcher owner=markdownlint::'
 
-if [ "${GITHUB_EVENT_NAME}" = pull_request ]; then
-  comment=""
-  FILES=$(echo "${OUTPUT}" | cut -d: -f1 | sort | uniq)
-
-  for file in ${FILES}; do
-    comment="${comment}<details><summary><code>${file}</code></summary>
-
-\`\`\`
-$(echo "${OUTPUT}" | grep "^${file}:" | sed "s/^[^:]*:[[:space:]]*//")
-\`\`\`
-
-</details>"
-  done
-
-  COMMENT_BODY="#### Issues with Markdown/CommonMark files
-${comment}
-
-*Workflow: \`${GITHUB_WORKFLOW}\`, Action: \`${GITHUB_ACTION}\`*"
-  PAYLOAD=$(echo '{}' | jq --arg body "${COMMENT_BODY}" '.body = $body')
-  COMMENTS_URL=$(jq -r .pull_request.comments_url <"${GITHUB_EVENT_PATH}")
-
-  curl -sS \
-    -H "Authorization: token ${GITHUB_TOKEN}" \
-    -H 'Content-Type: application/json' \
-    -d "${PAYLOAD}" \
-    "${COMMENTS_URL}" >/dev/null
-fi
-
-exit ${SUCCESS}
+exit ${RC}

0fa4e7038524b19dee115d1be88c499338668fb3
Nick Osborn <nosborn@users.noreply.github.com>
2022-09-22 21:37:30 +0800

    Expose --dot option

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,20 @@
 
 MARKDOWNLINT=markdownlint
 MARKDOWNLINT="${MARKDOWNLINT}${INPUT_CONFIG_FILE:+ -c ${INPUT_CONFIG_FILE}}"
+MARKDOWNLINT="${MARKDOWNLINT}${INPUT_DOT:+ -d}"
 MARKDOWNLINT="${MARKDOWNLINT}${INPUT_IGNORE_FILES:+ -i ${INPUT_IGNORE_FILES}}"
 MARKDOWNLINT="${MARKDOWNLINT}${INPUT_IGNORE_PATH:+ -p ${INPUT_IGNORE_PATH}}"
 MARKDOWNLINT="${MARKDOWNLINT}${INPUT_RULES:+ -r ${INPUT_RULES}}"
 
-PROBLEM_MATCHER="$(mktemp -p "${GITHUB_WORKSPACE}")"
+PROBLEM_MATCHER="$(mktemp -p "${GITHUB_WORKSPACE:?}")"
 trap 'rm -f "${PROBLEM_MATCHER}"' EXIT
 cp /markdownlint-problem-matcher.json "${PROBLEM_MATCHER:?}" || exit
 echo "::add-matcher::${PROBLEM_MATCHER:?}"
 
 # shellcheck disable=SC2086
-${MARKDOWNLINT} ${INPUT_FILES}
+${MARKDOWNLINT} ${INPUT_FILES:?}
 readonly RC=$?
 
 echo '::remove-matcher owner=markdownlint::'
 
-exit ${RC}
+exit "${RC}"
